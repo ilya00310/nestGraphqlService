2a44476646c49956abcf578161e569df5d524912
Шихман Илья <109863997+ilya00310@users.noreply.github.com>
2025-08-20 16:12:41 +0500

    Delete prisma/migrations directory

--- prisma/migrations/20250820105855_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."User" (
-    "id" SERIAL NOT NULL,
-    "age" INTEGER NOT NULL,
-    "work" BOOLEAN NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
